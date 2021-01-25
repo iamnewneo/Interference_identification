ff7b55381fe750c78279be1beac98c484cd69e2c
Kunal Gusain <kunalgusain@Kunals-MacBook-Pro.local>
2021-01-24 23:35:30 -0500

    Fix

--- interf_ident/driver.py
+++ interf_ident/driver.py
@@ -3,8 +3,7 @@ from interf_ident import config
 from interf_ident.preprocessing.create_dataset import create_dataset
 from interf_ident.trainer.trainer import model_trainer
 from interf_ident.data_loader.data_loader import create_data_loader
-
-# from interf_ident.trainer.predict import evaluate_model
+from interf_ident.trainer.predict import evaluate_model
 
 
 def main():
@@ -20,10 +19,10 @@ def main():
     val_loader = create_data_loader(X_test, y_test, batch_size=config.BATCH_SIZE)
     
     trainer = model_trainer(train_loader, val_loader, progress_bar_refresh_rate=0)
-    # model = trainer.get_model()
-    # result = evaluate_model(model, data_loader=val_loader)
-    # print(f"Test Loss: {result['loss']:.2f}")
-    # print(f"Test Accuracy: {result['accuracy']:.2f}")
+    model = trainer.get_model()
+    result = evaluate_model(model, data_loader=val_loader)
+    print(f"Test Loss: {result['loss']:.2f}")
+    print(f"Test Accuracy: {result['accuracy']:.2f}")
 
 
 if __name__ == "__main__":
