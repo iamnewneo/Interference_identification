c8c272b71ec16a061042f3787fa7b8a91bd4b541
Kunal Gusain <kunalgusain@Kunals-MacBook-Pro.local>
2021-01-24 23:19:06 -0500

    Fix

--- interf_ident/config.py
+++ interf_ident/config.py
@@ -1,13 +1,13 @@
 import os
 
-CLASSES = ["CI", "CWI", "MCWI", "SOI"]
+CLASSES = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 N_FOLDS = 5
 SEED = 42
 BASE_PATH = os.getenv("BASE_PATH", ".")
-# DEVICE = "cuda"
-# BATCH_SIZE = 100
-# MAX_EPOCHS = 25
-DEVICE = "cpu"
-BATCH_SIZE = 5
-MAX_EPOCHS = 3
 LR = 1e-3
+DEVICE = "cuda"
+BATCH_SIZE = 100
+MAX_EPOCHS = 25
+# DEVICE = "cpu"
+# BATCH_SIZE = 5
+# MAX_EPOCHS = 3
--- interf_ident/driver.py
+++ interf_ident/driver.py
@@ -11,16 +11,16 @@ def main():
 
     # create_dataset()
     print("Loading Data")
-    X_train = np.load(f"{config.BASE_PATH}/data/X_train.npy")[:100]
-    X_test = np.load(f"{config.BASE_PATH}/data/X_test.npy")[:100]
-    y_train = np.load(f"{config.BASE_PATH}/data/y_train.npy")[:100]
-    y_test = np.load(f"{config.BASE_PATH}/data/y_test.npy")[:100]
+    X_train = np.load(f"{config.BASE_PATH}/data/X_train.npy")
+    X_test = np.load(f"{config.BASE_PATH}/data/X_test.npy")
+    y_train = np.load(f"{config.BASE_PATH}/data/y_train.npy")
+    y_test = np.load(f"{config.BASE_PATH}/data/y_test.npy")
 
     train_loader = create_data_loader(X_train, y_train, batch_size=config.BATCH_SIZE)
     val_loader = create_data_loader(X_test, y_test, batch_size=config.BATCH_SIZE)
-    print("Starting Training")
+    
     trainer = model_trainer(train_loader, val_loader, progress_bar_refresh_rate=0)
-    model = trainer.get_model()
+    # model = trainer.get_model()
     # result = evaluate_model(model, data_loader=val_loader)
     # print(f"Test Loss: {result['loss']:.2f}")
     # print(f"Test Accuracy: {result['accuracy']:.2f}")
